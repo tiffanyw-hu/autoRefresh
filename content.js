675b69c02a5cb71f7ad22b65241a68e9330ecd57
Andrew Tae <tiffanyw.hu@gmail.com>
2018-06-13 23:10:46 -0700

    button syntax yea

--- content.js
+++ content.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('checkpage').onclick = function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.update(tabs[0].id, {url: "www.google.com"});
-    })
-  }
-})
+  document.getElementById('checkPage').onclick = function(tabs) {
+      chrome.tabs.update({url: "http://www.google.com"});
+    }
+  })
