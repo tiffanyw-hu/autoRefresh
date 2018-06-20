3e2dbdaa2db80dcf2d6882b91519394943417e78
Andrew Tae <tiffanyw.hu@gmail.com>
2018-06-19 18:59:21 -0700

    added css

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('checkPage').onclick = function(tabs) {
-    chrome.tabs.reload(tabs.id)
+    chrome.tabs.reload()
     }
   })
 
